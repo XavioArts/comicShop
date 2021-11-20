8dd47a57df1eb5896b4c9615cde816959b9cbdb9
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-11-20 13:51:39 -0700

    nested routes, started controllers

--- app/controllers/chaps_controller.rb
+++ app/controllers/chaps_controller.rb
@@ -1,2 +1,35 @@
 class ChapsController < ApplicationController
+
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+        
+    end
+
+    private
+
+    ##here will have get comic method and sanitize params
+
 end
