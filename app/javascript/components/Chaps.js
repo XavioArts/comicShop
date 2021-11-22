65e16ae22613c6afb3067d27c1d4141d92fdf96a
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-11-22 14:44:53 -0700

    chapters index working

---
+++ app/javascript/components/Chaps.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Chaps = (props) => {
+    const renderChaps = () => {
+        let count = 0;
+        return props.chaps.map((chap) => {
+            count++
+            return (
+                <div>
+                    <h3>{count}. <a href={`/comics/${props.comic.id}/chaps/${chap.id}`}>{chap.title}</a></h3>
+                    <p>{chap.sum}</p>
+                    <a href={`/comics/${props.comic.id}/chaps/${chap.id}/edit`}>Edit</a>
+                    <a href={`/comics/${props.comic.id}/chaps/${chap.id}`} data-method="delete">Delete</a>
+                </div>
+            );
+        });
+    }
+    return (
+        <div>
+            <h1>{props.comic.title}</h1>
+            <h3>Written by {props.comic.author}</h3>
+            <p>Synopsis: {props.comic.syn}</p>
+            <a href="/comics">Back to home</a>
+            {renderChaps()}
+        </div>
+    );
+};
+
+export default Chaps;
