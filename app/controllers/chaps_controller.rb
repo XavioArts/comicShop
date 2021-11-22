62f1420455c724de4419c57380bf73ae618f6226
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-11-22 00:40:28 -0700

    filled in controller info

--- app/controllers/chaps_controller.rb
+++ app/controllers/chaps_controller.rb
@@ -1,34 +1,59 @@
 class ChapsController < ApplicationController
 
-    def index
+    before_action :set_comic
+    before_action :set_chap, only: [:show, :edit, :update, :destroy]
 
+    def index
+        render component: "Chaps", props: { comic: @comic, chaps: @comic.chaps }
     end
 
     def show
-
+        render component: "Chap", props: { comic: @comic, chap: @chap }
     end
 
     def new
-
+        render component: "ChapNew", props: { comic: @comic, chap: @chap }
     end
 
     def create
-
+        @chap = @comic.chaps.new(set_params)
+        if @chap.save
+            redirect_to [@comic, @chap]
+        else
+            render :new
+        end
     end
 
     def edit
-
+        render component: "ChapEdit", props: { comic:@comic, chap: @chap }
     end
 
     def update
-
+        if @chap.update(set_params)
+            redirect_to [@comic, @chap]
+        else
+            render :edit
+        end
     end
 
     def destroy
-        
+        @chap.destroy
+        redirect_to comic_chaps_path(@comic)
     end
 
     private
+
+    def set_comic
+        @comic = Comic.find(params[:comic_id])
+    end
+
+    def set_chap
+        @chap = @comic.chaps.find(params[:id])
+    end
+
+    def set_params
+        params.require(:chap).permit(:title, :sum)
+    end
 
     ##here will have get comic method and sanitize params
 
--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,34 +1,61 @@
 class ComicsController < ApplicationController
 
-    def index
+    before_action :set_comic, only: [:show, :update, :edit, :destroy]
+    before_action :get_comics
 
+    def index
+        render component: "Comics", props: { comics: @comics }
     end
 
     def show
-
+        authenticity_token: session[:_csrf_token]
+        render component: "Comic", props: { comic: @comic, chaps: @comic.chaps, authenticity_token: session[:_csrf_token] }
+        ## trying out using the authenticity token
     end
 
     def new
-
+        render component: "ComicNew", props: {comic: @comic }
     end
 
     def create
-
+        @comic = Comic.new(set_params)
+        if @sub.save
+            redirect_to comics_path
+        else
+            render :new
+        end
     end
 
     def edit
-
+        render component: "ComicEdit", props: {comic: @comic}
     end
 
     def update
-
+        if @comic.update(set_params)
+            redirect_to @comic
+        else
+            render :edit
+        end
     end
 
     def destroy
-        
+        @comic.destroy
+        redirect_to comics_path        
     end
 
     private
+
+    def set_comic
+        @comic = Comic.find(params[:id])
+    end
+
+    def get_comics
+        @comics = Comic.all
+    end
+
+    def set_params
+        params.require(:comic).permit(:title, :author, :sum, :syn)
+    end
 
     ##here will have get comic method and sanitize params
 
