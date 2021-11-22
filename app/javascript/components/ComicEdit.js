4d4f0366dfc8f748389b5fe6bcfd400f346aa7df
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-11-22 13:00:31 -0700

    crud done on comic

--- app/javascript/components/ComicEdit.js
+++ app/javascript/components/ComicEdit.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 const ComicEdit = (props) => {
-
+    return (
+        <div>
+            <h1>Edit {props.comic.title}</h1>
+            <a href={`/comics/${props.comic.id}`}>Back to comic</a>
+            <form action={`/comics/${props.comic.id}`} method="post">
+                <input type="hidden" name="_method" value="patch" />
+                <p>Comic title:</p>
+                <input name="comic[title]" defaultValue={props.comic.title} />
+                <p>Comic author:</p>
+                <input name="comic[author]" defaultValue={props.comic.author} />
+                <p>Comic summary:</p>
+                <input name="comic[sum]" defaultValue={props.comic.sum} />
+                <p>Comic synopsis:</p>
+                <input name="comic[syn]" defaultValue={props.comic.syn} />
+                <button type="submit">Save Changes</button>
+            </form>
+        </div>
+    );
 };
 
 export default ComicEdit;
